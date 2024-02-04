a3ff12b0b76e189395009b7563a820c0f9d3a445
Vaibhavifreelance <158221175+Vaibhavifreelance@users.noreply.github.com>
2024-02-04 09:34:33 +0000

    comite changes

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const score = 400
-console.log(score);
+
+const mySym = Symbol("key1")
+
+
+const jsUser ={
+    [mySym]: "mykey1",
+    name: "vaibhavi",
+    "full name": "vaibhavi tiwari",
+    age: 16 ,
+    location: "kanpur",
+    email: "vaibhavi@google.com",
+    isLoggendIn: true,
+    lastLoginDays: ["monday", "sunday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(jsUser[mySym]);
+
+jsUser.email = "vibhavi@github.com"
+Object.freeze(jsUser)
+jsUser.email = "vibhavi@microsoft.com"
+console.log(jsUser);
